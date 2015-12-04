a9755b30fee05e6ea662bd9ade516a8f48beda04
Max Odnovolyk <modnovolyk@gmail.com>
2015-12-04 12:59:36 +0200

    Load all Service settings from server

--- Civilbudget/Classes/Project Details/ProjectDetailsViewModel.swift
+++ Civilbudget/Classes/Project Details/ProjectDetailsViewModel.swift
@@ -101,8 +101,12 @@ class ProjectDetailsViewModel {
         firstly {
             getSettings()
         }.then { (settings: ServiceSettings) -> Void in
-            BankIdSDK.Service.configuration.baseAuthURLString = settings.bankIdAuthURL
-            BankIdSDK.Service.configuration.redirectURI = settings.bankIdRedirectURI
+            var configuaration = BankIdSDK.Service.configuration
+            configuaration.clientID = settings.bankIdClientId
+            configuaration.baseAuthURLString = settings.bankIdAuthURL
+            configuaration.redirectURI = settings.bankIdRedirectURI
+            Service.configuration = configuaration
+            
             if !User.warningWasShownBefore {
                 self.infoAlertWithDescription.value = "BankID – це спосіб ідентифікації громадян.\nПри ідентифікації громадян через BankID НЕ ПЕРЕДАЄТЬСЯ фінансова та будь-яка інша приватна інформація. Тільки та інформація, що надається в паперовій формі при голосуванні за проекти Громадський Бюджет (ім’я, прізвище, по-батькові, стать, дата народження, адреса та ідентифікаційний номер).\nОтримані дані використовуються лише для ідентифікації громадян."
             }
